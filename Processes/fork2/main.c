c5f816cce21885d142a29deb31be3d6603d33cfd
CJQuintero61 <cjquintero61@gmail.com>
2026-02-03 14:24:04 -0600

    added getppid()

--- Processes/fork2/main.c
+++ Processes/fork2/main.c
@@ -16,6 +16,10 @@ int main() {
     else if (child_pid == 0) {
         // child process code
         printf("I am a child process and my PID is: %d\n", getpid());
+
+        // if the parent ends first, the child gets assigned to another process
+        // so getppid() may not match the parents pid
+        printf("I am a child process and my parent's ID is : %d\n", getppid());
     }
     else {
         // parent process code
