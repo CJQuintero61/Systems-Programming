58bb2600231d3ab1468e8a24f09daa5ff73571a3
CJQuintero61 <cjquintero61@gmail.com>
2026-02-03 13:49:09 -0600

    added uid and gid

--- Processes/process_id/main.c
+++ Processes/process_id/main.c
@@ -7,5 +7,14 @@ int main() {
     printf("My process ID is: %ld\n", (long)(getpid()));
     printf("My parent process ID is: %ld\n", (long)(getppid()));
     
+    pid_t effective_uid = geteuid();
+    pid_t effective_gid = getegid();
+    pid_t uid = getuid();
+    pid_t gid = getgid();
+
+    printf("Effective UID: %ld\n", (long)(effective_uid));
+    printf("Effective GID: %ld\n", (long)(effective_gid));
+    printf("UID: %ld\n", (long)(uid));
+    printf("GID: %ld\n", (long)(gid));
     return 0;
 }
